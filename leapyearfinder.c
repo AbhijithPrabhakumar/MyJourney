1ba13f43a0009a6d68f1d4fcae4b44190ce30207
AbhijithPrabhakumar <92842405+AbhijithPrabhakumar@users.noreply.github.com>
2021-10-20 11:01:46 +0530

    Update leapyearfinder.c

--- leapyearfinder.c
+++ leapyearfinder.c
@@ -1,1 +1,21 @@
-#include <stdio.h>
+#include <stdio.h> // To include C standard library for printf and scanf
+
+void leapYear() // Determines new function for finding leap year
+{
+    int year, leap; // Declare 2 variables for storing user input, and leap year checking
+    scanf("%i", &year); // Ask user for year
+    
+    leap = year % 4;
+    if(leap == 0)  // Check if leap variable is equal to 0
+    {
+        printf("%i is a leap year\n", year);  // Informs that this is a leap year if the condition is true
+    }
+    else
+    {
+        printf("%i is not a leap year\n", year);  // Else, informs it's not
+    }
+}
+int main()  // Control begins here
+{
+     leapYear(); // Call the function
+}
